1c4969c9f0e853fb7caf64449b2c8587a1a4d2c9
Narayan Aryal <aryal.coding@gmail.com>
2022-12-31 22:53:57 +1100

    server created

--- api/server.js
+++ api/server.js
@@ -1,2 +1,42 @@
-import  express from 'express'
+import express from "express";
 
+const app = express();
+
+const PORT = 8000;
+
+app.use("/", (req, res, next) => {
+  res.json({
+    status: "200 OK",
+    message: "Success",
+  });
+});
+app.get("/", (req, res, next) => {
+  res.json({
+    status: "200 OK",
+    message: "Success",
+  });
+});
+app.post("/", (req, res, next) => {
+  res.json({
+    status: "200 OK",
+    message: "posted",
+  });
+});
+app.put("/", (req, res, next) => {
+  res.json({
+    status: "200 OK",
+    message: "updated",
+  });
+});
+app.delete("/", (req, res, next) => {
+  res.json({
+    status: "200 OK",
+    message: "deleted",
+  });
+});
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`your server running at http://localhost:${PORT}`);
+});
