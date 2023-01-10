86f98baf953a6e3147c15fb76414f27d66ce7bbf
Narayan Aryal <aryal.coding@gmail.com>
2023-01-10 12:31:59 +1100

    code removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,10 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-import { SearchForm } from "./components/SearchForm";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useEffect, useState } from "react";
-import { getUser } from "./helper/axiosHelper";
-import { UserTable } from "./components/UserTable.js";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    getUsers();
-  }, []);
-
-  const getUsers = async () => {
-    const { result } = await getUser();
-    setUsers(result);
-  };
-  console.log(users);
   return (
     <div className="">
-      <section className="form">
-        <Container>
-          <SearchForm getUsers={getUsers} />
-          <hr />
-        </Container>
-      </section>
-      <section className="table"></section>
-      <Container>
-        {users.length} user found
-        <UserTable users={users} getUsers={getUsers} />
-      </Container>
-      <ToastContainer />
+      <h1>Hello World</h1>
     </div>
   );
 }
